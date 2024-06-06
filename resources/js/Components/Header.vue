74a48079e20c3985292edc55be9262fa2305c419
Daniel Martínez <danielmartinezisc98@outlook.com>
2024-06-05 21:54:27 -0600

    Se crearon los permisos

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -78,48 +78,50 @@ const items = ref([
             {
                 label: "Roles",
                 icon: "pi pi-key",
-                command: () => {
-                    Inertia.visit(route("role.index"));
-                },
+                command: () => Inertia.visit(route("role.index")),
+                // visible: auth.permissions.includes('role.index')
             },
             {
                 label: "Permisos",
                 icon: "pi pi-unlock",
-                command: () => {
-                    Inertia.visit(route("permission.index"));
-                },
+                command: () => Inertia.visit(route("permission.index")),
+                // visible: auth.permissions.includes('permission.index')
             },
             {
                 label: "Empresas asociadas",
                 icon: "pi pi-unlock",
-                command: () => {
-                    Inertia.visit(route("empresas.index"));
-                },
+                command: () => Inertia.visit(route("empresas.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Países",
                 icon: "pi pi-unlock",
                 command: () => Inertia.visit(route("paises.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Estados",
                 icon: "pi pi-unlock",
                 command: () => Inertia.visit(route("estados.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Municipios",
                 icon: "pi pi-unlock",
                 command: () => Inertia.visit(route("municipios.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Tipos de anuncios",
                 icon: "pi pi-unlock",
                 command: () => Inertia.visit(route("tiposanuncios.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Anuncios",
                 icon: "pi pi-unlock",
                 command: () => Inertia.visit(route("anuncios.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
         ],
     },
@@ -130,22 +132,26 @@ const items = ref([
             {
                 label: "Periodos",
                 icon: "bi bi-calendar3",
-                command: () => Inertia.visit(route("Calendarizaciones.Periodos.index"))
+                command: () => Inertia.visit(route("Calendarizaciones.Periodos.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Tipos de periodos",
                 icon: "bi bi-calendar2-check",
                 command: () => Inertia.visit(route("Calendarizaciones.TiposPeriodo.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Tipos de convocatorias",
                 icon: "bi bi-pencil-square",
                 command: () => Inertia.visit(route("Calendarizaciones.TiposConvocatoria.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Convocatorias",
                 icon: "bi bi-files-alt",
                 command: () => Inertia.visit(route("Calendarizaciones.Convocatorias.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
         ],
     },
@@ -156,17 +162,20 @@ const items = ref([
             {
                 label: "Aulas",
                 icon: "bi bi-easel2",
-                command: () => Inertia.visit(route("EstructuraAcademica.Aulas.index"))
+                command: () => Inertia.visit(route("EstructuraAcademica.Aulas.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Aulas Reservación",
                 icon: "bi bi-calendar-check",
-                command: () => Inertia.visit(route("EstructuraAcademica.AulasReservacion.index"))
+                command: () => Inertia.visit(route("EstructuraAcademica.AulasReservacion.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Grupos",
                 icon: "bi bi-people",
-                command: () => Inertia.visit(route("EstructuraAcademica.Grupos.index"))
+                command: () => Inertia.visit(route("EstructuraAcademica.Grupos.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
         ],
     },
@@ -177,32 +186,38 @@ const items = ref([
             {
                 label: "Carreras",
                 icon: "bi bi-mortarboard-fill",
-                command: () => Inertia.visit(route("GestionAcademica.Carreras.index"))
+                command: () => Inertia.visit(route("GestionAcademica.Carreras.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Mapas Curriculares",
                 icon: "bi bi-calendar2-range",
-                command: () => Inertia.visit(route("GestionAcademica.MapasCurriculares.index"))
+                command: () => Inertia.visit(route("GestionAcademica.MapasCurriculares.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Materias",
                 icon: "pi pi-book",
-                command: () => Inertia.visit(route("GestionAcademica.Materias.index"))
+                command: () => Inertia.visit(route("GestionAcademica.Materias.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Planes de estudio",
                 icon: "bi bi-journal-bookmark",
-                command: () => Inertia.visit(route("GestionAcademica.PlanesEstudios.index"))
+                command: () => Inertia.visit(route("GestionAcademica.PlanesEstudios.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Asesorías",
                 icon: "bi bi-journal-bookmark",
-                command: () => Inertia.visit(route("GestionAcademica.Asesorias.index"))
+                command: () => Inertia.visit(route("GestionAcademica.Asesorias.index")),
+                // visible: auth.permissions.includes('empresas.index')
             },
             {
                 label: "Asignar materias",
                 icon: "bi bi-journal-bookmark",
-                command: () => Inertia.visit(route("GestionAcademica.GrupoMateria.index"))
+                command: () => Inertia.visit(route("GestionAcademica.GrupoMateria.index")),
+                // visible: auth.permissions.includes('empresas.index')
             }
         ]
     },
